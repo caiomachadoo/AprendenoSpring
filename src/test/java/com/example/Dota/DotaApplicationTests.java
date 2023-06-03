8d2787f3a2d454168bd31e91ab33fd7e7ef31c25
Caio Machado <marquess.machado@gmail.com>
2023-06-02 21:34:48 -0300

    Projeto criado

---
+++ src/test/java/com/example/Dota/DotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
